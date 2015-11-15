09406e31a2c50f087f96a02af5fc1b1e5e22407f
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-11-15 21:57:25 +0200

    Refactored views to be under Server.

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ mongoose.connection.on('error', function() {
  * Express configuration.
  */
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Server/Views'));
 app.set('view engine', 'jade');
 app.use(compress());
 app.use(sass({
