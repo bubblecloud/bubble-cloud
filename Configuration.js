4c09301ccffbfe85462128d8731f6134563ca367
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-11-21 22:31:23 +0200

    Fixed page title.

--- Configuration.js
+++ Configuration.js
@@ -1,8 +1,8 @@
 function getConfiguration() {
     return {
-        'port': '3001',
+        'port': '3000',
         'remoteServers': [
-            { 'url': 'http://127.0.0.1:3000/', 'x': 0, 'y': 0, 'z': 3 }
+            { 'url': 'http://127.0.0.1:3001/', 'x': 0, 'y': 0, 'z': 3 }
         ],
         'databaseUrl': process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test'
     };
--- Configuration.ts
+++ Configuration.ts
@@ -1,8 +1,8 @@
 export function getConfiguration() : {[key: string]: any} {
     return {
-        'port': '3001',
+        'port': '3000',
         'remoteServers': [
-            { 'url': 'http://127.0.0.1:3000/', 'x':0, 'y':0, 'z':3 }
+            { 'url': 'http://127.0.0.1:3001/', 'x':0, 'y':0, 'z':3 }
         ],
         'databaseUrl': process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test'
     };
