f1ccf27acc009db4f6011cce342a8a5c8f6c4ee5
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-11-28 23:45:37 +0200

    Fixing string constants.

--- server/Web/Controllers/user.js
+++ server/Web/Controllers/user.js
@@ -265,8 +265,8 @@ exports.postReset = function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
-        subject: 'Your Hackathon Starter password has been changed',
+        from: 'client@bubblecloud.org',
+        subject: 'Your bubble cloud password has been changed',
         text: 'Hello,\n\n' +
           'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
       };
@@ -340,7 +340,7 @@ exports.postForgot = function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
+        from: 'client@bubblecloud.org',
         subject: 'Reset your password on Hackathon Starter',
         text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
