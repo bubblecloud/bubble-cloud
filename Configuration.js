862a488f42b80288987d7ab3714e2f5da14698a7
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-12-05 23:48:58 +0200

    Tuning colors.

--- Configuration.js
+++ Configuration.js
@@ -2,7 +2,7 @@ function getConfiguration() {
     return {
         'port': '3000',
         'remoteServers': [
-            { 'url': 'http://127.0.0.1:3001/', 'x': 0, 'y': 0, 'z': 3 }
+            { 'url': 'http://127.0.0.1:3001/', 'x': 8, 'y': -5, 'z': -15 }
         ],
         'databaseUrl': process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test'
     };
