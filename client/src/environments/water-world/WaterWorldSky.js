c43bd207f0995c627523b131a22d19a6811a7795
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-12-08 21:25:33 +0200

    Implemented water.

--- client/src/environments/water-world/WaterWorldSky.js
+++ client/src/environments/water-world/WaterWorldSky.js
@@ -23,6 +23,9 @@ var WaterWorldSky = (function () {
         skybox.renderingGroupId = 0;
         skybox.material = skyboxMaterial;
         var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        light1.diffuse = new BABYLON.Color3(1, 1, 1);
+        light1.specular = new BABYLON.Color3(0, 0, 0);
+        light1.groundColor = new BABYLON.Color3(0, 0, 0);
         light1.parent = skybox;
         var light = new BABYLON.PointLight("sun" + entity.id, new BABYLON.Vector3(-17.6, 18.8, -49.9), scene);
         light.parent = skybox;
@@ -34,13 +37,29 @@ var WaterWorldSky = (function () {
         new BABYLON.LensFlare(0.1, 0.6, new BABYLON.Color3(1, 1, 1), "images/effects/Flare.png", lensFlareSystem);
         new BABYLON.LensFlare(0.3, 0.8, new BABYLON.Color3(1, 1, 1), "images/effects/Flare.png", lensFlareSystem);
         engine.renderer.avatarAttachments.push(entity);
-        this.createWaterTestScene(engine, skybox);
+        var waterMaterial = new BABYLON.WaterMaterial("waterMaterial", scene, new BABYLON.Vector2(512, 512));
+        waterMaterial.bumpTexture = new BABYLON.Texture("images/bump-maps/waterbump.png", scene);
+        waterMaterial.windForce = -0.5;
+        waterMaterial.waveHeight = 0.0;
+        waterMaterial.bumpHeight = 0.05;
+        waterMaterial.waveLength = 0.1;
+        waterMaterial.waveSpeed = 0.1;
+        waterMaterial.colorBlendFactor = 0;
+        waterMaterial.windDirection = new BABYLON.Vector2(1, 1);
+        waterMaterial.colorBlendFactor = 0;
+        var waterMesh = BABYLON.Mesh.CreateGround("water" + entity.id, 5000, 5000, 32, scene, false);
+        waterMesh.parent = skybox;
+        waterMesh.position.y = -100;
+        waterMesh.renderingGroupId = 0;
+        waterMesh.material = waterMaterial;
+        waterMaterial.addToRenderList(skybox);
     };
     WaterWorldSky.prototype.remove = function (engine, entity) {
         var scene = engine.renderer.scene;
         var mesh = scene.getMeshByName(entity.id);
         if (mesh) {
             scene.removeLight(scene.getLightByName("sun" + entity.id));
+            scene.removeMesh(scene.getMeshByName("water" + entity.id));
             scene.removeMesh(mesh);
         }
         if (engine.renderer.avatarAttachments.indexOf(entity) >= 0) {
@@ -53,25 +72,6 @@ var WaterWorldSky = (function () {
         if (mesh) {
             mesh.position = engine.avatarController.avatar.position;
         }
-    };
-    WaterWorldSky.prototype.createWaterTestScene = function (clientEngine, skybox) {
-        var scene = clientEngine.renderer.scene;
-        var waterMaterial = new BABYLON.WaterMaterial("waterMaterial", scene, new BABYLON.Vector2(512, 512));
-        waterMaterial.bumpTexture = new BABYLON.Texture("images/bump-maps/waterbump.png", scene);
-        waterMaterial.windForce = -0.5;
-        waterMaterial.waveHeight = 0.0;
-        waterMaterial.bumpHeight = 0.05;
-        waterMaterial.waveLength = 0.1;
-        waterMaterial.waveSpeed = 0.1;
-        waterMaterial.colorBlendFactor = 0;
-        waterMaterial.windDirection = new BABYLON.Vector2(1, 1);
-        waterMaterial.colorBlendFactor = 0;
-        var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 5000, 5000, 32, scene, false);
-        waterMesh.parent = skybox;
-        waterMesh.position.y = -100;
-        waterMesh.renderingGroupId = 0;
-        waterMesh.material = waterMaterial;
-        waterMaterial.addToRenderList(skybox);
     };
     return WaterWorldSky;
 })();
--- client/src/environments/water-world/WaterWorldSky.ts
+++ client/src/environments/water-world/WaterWorldSky.ts
@@ -40,7 +40,9 @@ export class WaterWorldSky implements Actuator {
         skybox.material = skyboxMaterial;
 
         var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-       //light1.position = new BABYLON.Vector3(0, -100, 0);
+        light1.diffuse = new BABYLON.Color3(1, 1, 1);
+        light1.specular = new BABYLON.Color3(0, 0, 0);
+        light1.groundColor = new BABYLON.Color3(0, 0, 0);
         light1.parent = skybox;
 
         // Add sun.
@@ -59,7 +61,25 @@ export class WaterWorldSky implements Actuator {
 
         engine.renderer.avatarAttachments.push(entity);
 
-        this.createWaterTestScene(engine, skybox);
+        var waterMaterial = new BABYLON.WaterMaterial("waterMaterial", scene, new BABYLON.Vector2(512, 512));
+        waterMaterial.bumpTexture = new BABYLON.Texture("images/bump-maps/waterbump.png", scene);
+        waterMaterial.windForce = -0.5;
+        waterMaterial.waveHeight = 0.0;
+        waterMaterial.bumpHeight = 0.05;
+        waterMaterial.waveLength = 0.1;
+        waterMaterial.waveSpeed = 0.1;
+        waterMaterial.colorBlendFactor = 0;
+        waterMaterial.windDirection = new BABYLON.Vector2(1, 1);
+        waterMaterial.colorBlendFactor = 0;
+
+        // Water mesh
+        var waterMesh = BABYLON.Mesh.CreateGround("water" + entity.id, 5000, 5000, 32, scene, false);
+        waterMesh.parent = skybox;
+        waterMesh.position.y = - 100;
+        waterMesh.renderingGroupId = 0;
+        waterMesh.material = waterMaterial;
+
+        waterMaterial.addToRenderList(skybox);
     }
 
     remove(engine: ClientEngine, entity: ClientEntity): void {
@@ -67,6 +87,7 @@ export class WaterWorldSky implements Actuator {
         var mesh = scene.getMeshByName(entity.id);
         if (mesh) {
             scene.removeLight(scene.getLightByName("sun" + entity.id));
+            scene.removeMesh(scene.getMeshByName("water" + entity.id));
             scene.removeMesh(mesh);
         }
         if (engine.renderer.avatarAttachments.indexOf(entity) >= 0) {
@@ -83,69 +104,4 @@ export class WaterWorldSky implements Actuator {
         }
     }
 
-    createWaterTestScene(clientEngine: ClientEngine, skybox: BABYLON.Mesh): void {
-        var scene = clientEngine.renderer.scene;
-        // Camera
-        //var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), scene);
-
-        // Light
-        //var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-        /*
-        // Skybox
-        var skybox = BABYLON.Mesh.CreateBox("skyBox", 5000.0, scene);
-        var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-        skyboxMaterial.backFaceCulling = false;
-        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("../../assets/skybox/TropicalSunnyDay", scene);
-        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-        skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-        //skyboxMaterial.disableLighting = true;
-        skybox.material = skyboxMaterial;*/
-
-        // Water material
-        var waterMaterial = new BABYLON.WaterMaterial("waterMaterial", scene, new BABYLON.Vector2(512, 512));
-        waterMaterial.bumpTexture = new BABYLON.Texture("images/bump-maps/waterbump.png", scene);
-        waterMaterial.windForce = -0.5;
-        waterMaterial.waveHeight = 0.0;
-        waterMaterial.bumpHeight = 0.05;
-        waterMaterial.waveLength = 0.1;
-        waterMaterial.waveSpeed = 0.1;
-        waterMaterial.colorBlendFactor = 0;
-        waterMaterial.windDirection = new BABYLON.Vector2(1, 1);
-        waterMaterial.colorBlendFactor = 0;
-
-        // Ground
-
-        /*var groundTexture = new BABYLON.Texture("images/textures/sand.jpg", scene);
-        groundTexture.vScale = groundTexture.uScale = 4.0;
-
-        var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-        groundMaterial.diffuseTexture = groundTexture;
-
-        var ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene, false);
-        ground.position.y = -21;
-        ground.material = groundMaterial;*/
-
-        // Water mesh
-        var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 5000, 5000, 32, scene, false);
-        waterMesh.parent = skybox;
-        waterMesh.position.y = - 100;
-        waterMesh.renderingGroupId = 0;
-
-        waterMesh.material = waterMaterial;
-
-        // Sphere
-        /*var sphereMaterial = new BABYLON.StandardMaterial("sphereMaterial", scene);
-        sphereMaterial.diffuseTexture = new BABYLON.Texture("../../assets/wood.jpg", scene);*/
-
-        /*var sphere = BABYLON.Mesh.CreateSphere("sphere", 32, 24, scene);
-        sphere.position.y = 20;
-        sphere.material = sphereMaterial;*/
-
-        // Configure water material
-        //waterMaterial.addToRenderList(ground);
-        waterMaterial.addToRenderList(skybox);
-        //waterMaterial.addToRenderList(sphere);
-    }
 }
