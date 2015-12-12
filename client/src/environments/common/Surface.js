bb748b355e7d06507c77022ff364d3316a6e4b2c
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-12-12 08:43:51 +0200

    Implementing parametrized surface mesh type.

--- client/src/environments/common/Surface.js
+++ client/src/environments/common/Surface.js
@@ -1,9 +1,18 @@
+var ClientEntity_1 = require("../../components/ClientEntity");
 var Surface = (function () {
     function Surface() {
         this.repository = 'default';
         this.environment = 'common';
         this.type = 'surface';
     }
+    Surface.prototype.construct = function () {
+        var newEntity = new ClientEntity_1.PrimitiveEntity();
+        newEntity.newId();
+        newEntity.repo = this.repository;
+        newEntity.type = this.type;
+        newEntity.dynamic = true;
+        return newEntity;
+    };
     Surface.prototype.add = function (engine, entity) {
         var scene = engine.renderer.scene;
         var mesh = this.createSurface(entity.id, scene);
@@ -20,17 +29,22 @@ var Surface = (function () {
     Surface.prototype.update = function (engine, entity) {
     };
     Surface.prototype.createSurface = function (name, scene) {
-        var ni = 10;
-        var nj = 10;
-        var li = 2;
-        var lj = 2;
+        var ni = 20;
+        var nj = 20;
+        var li = 4;
+        var lj = 4;
         var paths = [];
-        for (var j = 0; j < nj; j++) {
+        for (var jj = 0; jj < nj; jj++) {
             var path = [];
-            for (var i = 0; i < ni; i++) {
-                path.push(new BABYLON.Vector3(li * i / ni - li / 2, lj * j / nj - lj / 2, 0));
+            for (var ii = 0; ii < ni; ii++) {
+                var i = ii / ni;
+                var j = jj / nj;
+                var x = li * i;
+                var y = lj * j;
+                var z = Math.cos(2 * Math.PI * (i - j)) / 5;
+                path.push(new BABYLON.Vector3(x, y, z));
             }
-            paths[j] = path;
+            paths.push(path);
         }
         return BABYLON.Mesh.CreateRibbon(name, paths, false, false, 0, scene, false, BABYLON.Mesh.FRONTSIDE);
     };
--- client/src/environments/common/Surface.ts
+++ client/src/environments/common/Surface.ts
@@ -10,6 +10,15 @@ export class Surface implements Actuator {
     environment: string = 'common';
     type: string = 'surface';
 
+    construct():ClientEntity {
+        var newEntity: PrimitiveEntity = new PrimitiveEntity();
+        newEntity.newId();
+        newEntity.repo = this.repository;
+        newEntity.type = this.type;
+        newEntity.dynamic = true;
+        return newEntity;
+    }
+
     add(engine: ClientEngine, entity: ClientEntity): void {
         var scene: Scene = engine.renderer.scene;
         var mesh = this.createSurface(entity.id, scene);
@@ -31,18 +40,27 @@ export class Surface implements Actuator {
 
     createSurface(name: string, scene: Scene): AbstractMesh {
 
-        var ni = 10;
-        var nj = 10;
-        var li = 2;
-        var lj = 2;
+        var ni = 20;
+        var nj = 20;
+        var li = 4;
+        var lj = 4;
 
         var paths = [];
-        for (var j = 0; j < nj; j++) {
+        for (var jj = 0; jj < nj; jj++) {
             var path = [];
-            for (var i = 0; i < ni; i++) {
-                path.push(new BABYLON.Vector3(li * i / ni - li / 2, lj * j / nj - lj / 2, 0));
+            for (var ii = 0; ii < ni; ii++) {
+                // Scale i and j to range of 0..1
+                var i = ii / ni;
+                var j = jj / nj;
+
+                // Define surface function
+                var x = li * i;
+                var y = lj * j;
+                var z = Math.cos(2 * Math.PI * (i-j))/ 5;
+
+                path.push(new BABYLON.Vector3(x, y, z));
             }
-            paths[j] = path;
+            paths.push(path);
         }
 
         return BABYLON.Mesh.CreateRibbon(name, paths, false, false, 0, scene, false, BABYLON.Mesh.FRONTSIDE);
