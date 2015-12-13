d66924b1dc329ebe6c6461141ffd06b1b3f4121f
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2015-12-13 12:34:39 +0200

    Implemented entity add with parent selected to retain position and rotation of avatar.

--- client/src/hud/entity-add.js
+++ client/src/hud/entity-add.js
@@ -1,4 +1,5 @@
 var hud_1 = require("../hud");
+var Matrix = BABYLON.Matrix;
 var EntityAdd = (function () {
     function EntityAdd() {
         this.entityTypes = [
@@ -11,14 +12,29 @@ var EntityAdd = (function () {
     EntityAdd.prototype.addEntity = function () {
         var actuator = this.engine.actuatorRegister.get('default', this.addEntityType);
         var newEntity = actuator.construct(this.engine);
+        var entityWorldPosition = this.engine.avatarController.avatar.position.clone();
+        var entityLocalPosition;
+        var entityWorldRotation = new Matrix();
+        this.engine.avatarController.avatar.rotationQuaternion.toRotationMatrix(entityWorldRotation);
+        var entityLocalRotation;
         var parentEntity = this.engine.state.getEditedEntity();
         if (parentEntity) {
             newEntity.pid = parentEntity.id;
             newEntity.prid = parentEntity.rid;
+            var mesh = this.engine.renderer.scene.getMeshByName(newEntity.pid);
+            var worldMatrix = mesh.getWorldMatrix();
+            var worldMatrixInverted = new Matrix();
+            worldMatrix.invertToRef(worldMatrixInverted);
+            entityLocalPosition = BABYLON.Vector3.TransformCoordinates(entityWorldPosition, worldMatrixInverted);
+            entityLocalRotation = entityWorldRotation.multiply(worldMatrixInverted);
+        }
+        else {
+            entityLocalPosition = entityWorldPosition;
+            entityLocalRotation = entityWorldRotation;
         }
         newEntity.dynamic = false;
-        newEntity.position = this.engine.avatarController.avatar.position.clone();
-        newEntity.rotationQuaternion = this.engine.avatarController.avatar.rotationQuaternion.clone();
+        newEntity.position = entityLocalPosition;
+        newEntity.rotationQuaternion.fromRotationMatrix(entityLocalRotation);
         this.engine.ws.sendObject(newEntity);
         this.engine.state.setEditedEntity(newEntity);
     };
--- client/src/hud/entity-add.ts
+++ client/src/hud/entity-add.ts
@@ -2,6 +2,8 @@ import {ConsoleController} from "../components/ConsoleController";
 import {getClientEngine} from "../hud";
 import {ClientEngine} from "../components/ClientEngine";
 import Quaternion = BABYLON.Quaternion;
+import Matrix = BABYLON.Matrix;
+import Vector3 = BABYLON.Vector3;
 
 export class EntityAdd {
     engine:ClientEngine;
@@ -20,16 +22,34 @@ export class EntityAdd {
         var actuator = this.engine.actuatorRegister.get('default', this.addEntityType);
         var newEntity = actuator.construct(this.engine);
 
+        var entityWorldPosition = this.engine.avatarController.avatar.position.clone();
+        var entityLocalPosition: Vector3;
+
+        var entityWorldRotation = new Matrix();
+        this.engine.avatarController.avatar.rotationQuaternion.toRotationMatrix(entityWorldRotation);
+        var entityLocalRotation: Matrix;
+
         // Set parent if one is selected.
         var parentEntity = this.engine.state.getEditedEntity();
         if  (parentEntity) {
             newEntity.pid = parentEntity.id;
             newEntity.prid = parentEntity.rid;
+
+            var mesh = this.engine.renderer.scene.getMeshByName(newEntity.pid);
+            var worldMatrix = mesh.getWorldMatrix();
+            var worldMatrixInverted = new Matrix();
+            worldMatrix.invertToRef(worldMatrixInverted);
+            entityLocalPosition = BABYLON.Vector3.TransformCoordinates(entityWorldPosition, worldMatrixInverted);
+
+            entityLocalRotation = entityWorldRotation.multiply(worldMatrixInverted);
+        } else {
+            entityLocalPosition = entityWorldPosition;
+            entityLocalRotation = entityWorldRotation;
         }
 
         newEntity.dynamic = false;
-        newEntity.position = this.engine.avatarController.avatar.position.clone();
-        newEntity.rotationQuaternion = this.engine.avatarController.avatar.rotationQuaternion.clone();
+        newEntity.position = entityLocalPosition;
+        newEntity.rotationQuaternion.fromRotationMatrix(entityLocalRotation);
         this.engine.ws.sendObject(newEntity);
         this.engine.state.setEditedEntity(newEntity);
 //        alert('add ' + this.addEntityType);
